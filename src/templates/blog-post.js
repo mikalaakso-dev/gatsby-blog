5b229bbee3b727206bbe4a732d5183d768b4eceb
mikalaakso-dev <laaksomik@gmail.com>
2025-04-11 14:21:40 +0300

    Added animation to blog post pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Clouds from "../components/clouds"
 
 const BlogPostTemplate = ({
   data: { previous, next, site, markdownRemark: post },
@@ -13,6 +14,7 @@ const BlogPostTemplate = ({
 
   return (
     <Layout location={location} title={siteTitle}>
+      <Clouds />
       <article
         className="blog-post"
         itemScope
